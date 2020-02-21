55a2dec304e7c1c594d6280e5cbb25cc6147ea76
nguyendangkhuong96 <nguyendang.khuong@asoview.vn>
2020-02-21 13:32:48 +0700

    paginnation

--- pages/api/products.js
+++ pages/api/products.js
@@ -4,6 +4,22 @@ import connectDb from '../../utils/connectDb'
 connectDb()
 
 export default async (req, res) => {
-  const products = await Product.find()
-  res.status(200).json(products)
+  const { page, size } = req.query
+  // Convert querystring values to number
+  const pageNum = Number(page)
+  const pageSize = Number(size)
+  console.log(pageSize)
+  let products = []
+  const totalDocs = await Product.countDocuments()
+  const totalPages = Math.ceil(totalDocs / pageSize)
+  console.log(totalDocs)
+  if (pageNum === 1) {
+    products = await Product.find().limit(pageSize)
+  } else {
+    const skip = pageSize * (pageNum - 1)
+    products = await Product.find().skip(skip).limit(pageSize)
+  }
+  console.log(totalPages)
+  // const products = await Product.find()
+  res.status(200).json({ products, totalPages })
 }
