4f0362ddd8fe497eb1cb65055e2240ad0a82be3e
nguyendangkhuong96 <nguyendang.khuong@asoview.vn>
2020-02-21 17:28:04 +0700

    formatDate

--- pages/api/orders.js
+++ pages/api/orders.js
@@ -1,6 +1,5 @@
 import Order from '../../models/Order'
 import jwt from 'jsonwebtoken'
-import products from './products'
 import connectDb from '../../utils/connectDb'
 
 connectDb()
@@ -9,6 +8,9 @@ export default async (req, res) => {
   try {
     const { userId } = jwt.verify(req.headers.authorization, process.env.JWT_SECRET)
     const orders = await Order.find({ user: userId })
+      .sort({
+        createdAt: 'desc' // short hand : 'desc' : -1, 'asc' : 1
+      })
       .populate({
         path: 'products.product',
         model: 'Product'
--- pages/api/users.js
+++ pages/api/users.js
@@ -9,6 +9,9 @@ export default async (req, res) => {
     const { userId } = jwt.verify(req.headers.authorization, process.env.JWT_SECRET)
     // get user not is current root user
     const users = await User.find({ _id: { $ne: userId } })
+      .sort({
+        name: 'asc'
+      })
     res.status(200).json(users)
   } catch (error) {
     console.log(error)
